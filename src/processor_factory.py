645ccd2161b39123e4f2605def5b37d6d686d9e2
Koki Nagamine <nagamine.k.aa@gmail.com>
2019-07-28 15:14:52 +0900

    update classifier process

--- src/processor_factory.py
+++ src/processor_factory.py
@@ -1,25 +1,20 @@
-import gc
 import yaml
 
 from logging import getLogger
 from pathlib import Path
+
+from classifier_process import classifier_process
+from util.command_option import get_version
 
 
 class ProcessorFactory():
     """
     Abstract Factory Pattern
     Return experiment process by fold.(Each process return only one model.)
-
-    Attributes
-    ----------
-    ROOT_PATH : PosixPath object
-        project root path.
     """
 
-    ROOT_PATH = Path(__file__).absolute().parents[1]
-
     @classmethod
-    def make_process(cls, version):
+    def make_process(self, fold):
         """
         Loading config and making process instance.
 
@@ -28,28 +23,38 @@ class ProcessorFactory():
         process : process instance
             training process instance.
         """
-        pass
+        config = self.__load_config()
+        print(config)
+
+        process = None
+
+        if config["task"] == "regression":
+            process = classifier_process(config)
+
+        return process
 
     @classmethod
-    def __load_config(cls, version):
+    def __load_config(self):
         """
         Loading yaml file.
-
-        Parameters
-        ----------
-        version : str
-            experiment version unique id.
 
         Returns
         -------
         config : dict
             information of process condition.
         """
-        pass
+        version = get_version()
+        yaml_filepath = Path(__file__).parents[1] / "config" / f"{version}.yml"
+
+        with open(yaml_filepath, "r") as f:
+            config_dict = yaml.safe_load(f)
+
+        return config_dict
 
 
+from process import process
 if __name__ == "__main__":
     """
     unit test
     """
-    pass
+    assert isinstance(ProcessorFactory.make_process(0), process)
