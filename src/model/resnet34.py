6bb7975c93781d9bfe55ba9dac0cda9942f91c7e
Koki Nagamine <nagamine.k.aa@gmail.com>
2019-07-30 00:55:47 +0900

    update model resnet18, resnet34

---
+++ src/model/resnet34.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+
+import torch
+import torch.nn as nn
+from torchvision import models
+
+
+def get_resnet34(task, weight=None, pretrained=False):
+    """
+    Parameters
+    ----------
+    task: str
+        kind of task.
+    weight: torch.dict
+        pretrained weight in training section.
+        so, this parameter use in inference.
+    pretrained: bool
+        load torchvision model weight.
+    """
+    resnet34 = models.resnet34(pretrained=False)
+    if pretrained:
+        resnet34.load_state_dict(torch.load(Path(__file__).parents[2] / "model" / "pretrain" / "resnet34.pth"))
+
+    if task == "classifier":
+        resnet34.fc = nn.Sequential(
+            nn.Linear(in_features=512, out_features=5),
+            nn.Sigmoid()
+        )
+    elif task == "regression":
+        resnet34.fc = nn.Sequential(
+            nn.Linear(in_features=512, out_features=1)
+        )
+    else:
+        print("{} task isn't implemented.".format(task))
+        raise NotImplementedError
+
+    if weight is not None:
+        resnet34.load_state_dict(weight)
+
+    return resnet34
+
+
+if __name__ == "__main__":
+    net = get_resnet34("classifier", pretrained=True)
+    x = torch.randn(16, 3, 512, 512)
+    y = net(x)
+    assert torch.Size([16, 5]) == y.size()
+
+    net = get_resnet34("regression", pretrained=True)
+    x = torch.randn(16, 3, 512, 512)
+    y = net(x)
+    assert torch.Size([16, 1]) == y.size()
+
+    print("complete")
